fcf3298d57b6422205888979bb4498b9c5f1ef90
Ryan May <xtheraingoddessx@gmail.com>
2017-06-05 00:14:06 -0700

    setup the main boxes that each part of the app will work in

--- Fiction Archive/src/com/rain/fiction_archive/gui/MainWindow.java
+++ Fiction Archive/src/com/rain/fiction_archive/gui/MainWindow.java
@@ -1,8 +1,19 @@
 package com.rain.fiction_archive.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application{
@@ -26,6 +37,99 @@ public class MainWindow extends Application{
 		AnchorPane mainComponentPane = new AnchorPane();
 		mainComponentPane.setMinWidth(WINDOW_MIN_WIDTH);
 		mainComponentPane.setMinHeight(WINDOW_MIN_HEIGHT);
+		mainComponentPane.getChildren().add(getMainVerticalBox());
 		return mainComponentPane;
+	}
+	
+	private VBox getMainVerticalBox(){
+		VBox vbox = new VBox();
+		vbox.getChildren().addAll(getMenuBar(), getMainBorderPane());
+		return vbox;
+	}
+	
+//MenuBar
+	private MenuBar getMenuBar(){
+		MenuBar menuBar = new MenuBar();
+        
+        // --- Menu File
+        Menu menuFile = getMenuFileOption();
+        
+        // --- Menu Edit
+        Menu menuEdit = getMenuEditOption();
+ 
+        // --- Menu View
+        Menu menuView = getMenuViewOption();
+        
+        menuBar.setMinWidth(WINDOW_MIN_WIDTH);
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        menuBar.autosize();
+        return menuBar;
+	}
+	
+	private Menu getMenuFileOption(){
+		Menu menu = new Menu("File");
+		
+		MenuItem add = getMenuItem("Add", new MenuAction(){
+			@Override
+			public void execute() {
+				
+			}
+		});
+		
+		MenuItem remove = getMenuItem("Remove", new MenuAction(){
+			@Override
+			public void execute() {
+				
+			}
+		});
+		
+	    menu.getItems().addAll(add, remove);
+	    return menu;
+	}
+	
+	private MenuItem getMenuItem(String name, MenuAction action){
+		MenuItem item = new MenuItem(name,
+            new ImageView(new Image("res/" + name.toLowerCase()+".png")));
+        item.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+			public void handle(ActionEvent t) {
+                action.execute();
+            }
+        });
+        return item;
+	}
+	
+	private Menu getMenuEditOption(){
+		Menu menu = new Menu("Edit");
+		return menu;
+	}
+	
+	private Menu getMenuViewOption(){
+		Menu menu = new Menu("View");
+		return menu;
+	}
+
+//Main border Pane
+	private BorderPane getMainBorderPane(){
+		BorderPane mainBorderPane = new BorderPane();
+		mainBorderPane.setTop(getArchiveControls());
+		mainBorderPane.setRight(getArchiveData());
+		mainBorderPane.setLeft(getArchiveFileSystem());
+		return new BorderPane();
+	}
+	
+//Archive controls
+	private HBox getArchiveControls(){
+		return new HBox();
+	}
+	
+//Archive Data
+	private TabPane getArchiveData(){
+		return new TabPane();
+	}
+	
+//Archive File System
+	private VBox getArchiveFileSystem(){
+		return new VBox();
 	}
 }
