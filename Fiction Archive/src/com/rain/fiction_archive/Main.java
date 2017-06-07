21ad48bbb5f44975e7cf13939eec82ee48d27c9d
Ryan May <xtheraingoddessx@gmail.com>
2017-06-06 21:13:48 -0700

    removed initial test cases

--- Fiction Archive/src/com/rain/fiction_archive/Main.java
+++ Fiction Archive/src/com/rain/fiction_archive/Main.java
@@ -16,9 +16,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import com.rain.fiction_archive.files.Fandom;
-import com.rain.fiction_archive.files.FandomAttributes;
-import com.rain.fiction_archive.files.Fiction;
-import com.rain.fiction_archive.files.FictionAttributes;
 import com.rain.fiction_archive.gui.MainWindow;
 
 /**
@@ -36,28 +33,34 @@ public class Main {
 	public static void main(String[] args) {
 		try{
 			createHomeDirectory();
-			loadArchiveData();
+			readFromDisk();
 			
+			/*
 			Fandom f1 = new Fandom(new FandomAttributes()
 											.setName("Clexa")
 											.setPath("C:\\Fandom\\Clexa.data"),
 											getNextUUID() );
 			f1.setFictions(Arrays.asList(new Fiction(new FictionAttributes()
 																.setName("Hedatu")
-																.setAuthor("Red Hope")),
+																.setAuthor("Red Hope")
+																.setWordCount(100000)),
 										 new Fiction(new FictionAttributes()
 												 				.setName("Before Words, Beyond Silence")
-												 				.setAuthor("natkate"))));
+												 				.setAuthor("natkate")
+										 						.setWordCount(389000))
+										 ));
 			masterArchiveData.put(f1.getUUID(), f1);
 			
 			Fandom f2 = new Fandom(new FandomAttributes()
 					.setName("Bellice")
 					.setPath("C:\\Fandom\\Bellice.data"),
 					getNextUUID() );
-			masterArchiveData.put(f2.getUUID(), f2);
+			masterArchiveData.put(f2.getUUID(), f2); */
 			
 			MainWindow app = new MainWindow();
 			app.begin(args);
+			
+			writeToDisk();
 			
 		} catch(IOException | ClassNotFoundException e){
 			e.printStackTrace();
@@ -71,6 +74,17 @@ public class Main {
 			homeDir.mkdirs();
 	}
 	
+	private static void writeToDisk() throws IOException{
+		List<Fandom> list = getMasterDataAsList();
+		for(int i = 0; i<list.size(); i++){
+			list.get(i).writeToDisk();
+		}
+	}
+	
+	private static void readFromDisk() throws FileNotFoundException, ClassNotFoundException, IOException{
+		loadArchiveData();
+	}
+	
 	private static void loadArchiveData() throws FileNotFoundException, IOException, ClassNotFoundException{
 		List<File> files = getFilesToLoad();
 		if(files!=null){	
@@ -78,23 +92,31 @@ public class Main {
 				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
 				Fandom fandom = (Fandom) ois.readObject();
 				fandom.setUUID(getNextUUID());
-				System.out.println("New media read from disk: \n" + fandom);
+				System.out.println("New fandom read from disk: \n" + fandom);
 				masterArchiveData.put(fandom.getUUID(), fandom);
 				ois.close();
+				file.delete();
 			}
 		}
 	}
 	
 	private static List<File> getFilesToLoad(){
-		File mainDirectory = new File(home + "Fandoms\\");
+		File mainDirectory = new File(home);
 		File[] files = mainDirectory.listFiles();
+		//System.out.println(files);
 		if(files!=null){
-			List<File> fandomDir = Arrays.asList(files);
+			List<File> mainDir = Arrays.asList(files);
 			List<File> allData = new ArrayList<>();
-			for (int k = 0; k < fandomDir.size(); k++) {
-				if(fandomDir.get(k).getAbsolutePath().contains(".data"))
-					allData.add(fandomDir.get(k));
-			} System.out.println("Files to be read into ram : \n" + allData); 
+			for (int k = 0; k < mainDir.size(); k++) {
+				if(mainDir.get(k).isDirectory()){
+					List<File> fandomDir = Arrays.asList(mainDir.get(k).listFiles());
+					for(int i = 0; i<fandomDir.size(); i++){
+						if(fandomDir.get(i).getAbsolutePath().contains(".data"))
+							allData.add(fandomDir.get(i));
+					}
+				}
+				
+			} System.out.println("Files to be read from disk into ram : \n" + allData + "\n"); 
 			return allData; 
 		} else{
 			return new ArrayList<File>();
